760a6d362601ffa4e79c7fbd9bae81bb1605eba9
Martin Schommertz <martinschommertz@googlemail.com>
2024-05-11 17:01:05 +0200

    In diesem Push wurde die Startseite mit 3 Buttons integriert und die Navigation auf die Seite "NewEntry" abgeschlossen

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Dieses Projekt wird für den Kurs "Projekt: Software Engineering" im Rahmen des Masters M. Sc. Informatik an der IU durchgeführt.
+Es handelt sich bei dieser Anwendung um ein Haushaltsbuch, um seine Finanzen im Überblick zu behalten
--- Window_NewEntry.xaml.cs
+++ Window_NewEntry.xaml.cs
@@ -1,6 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using ModernWpf.Controls;
-using System.Text;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,22 +8,22 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace MartinsHaushaltsbuch
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class Window_NewEntry : MetroWindow
     {
-        public MainWindow()
+        public Window_NewEntry()
         {
             InitializeComponent();
         }
-
-        private void Button_NewEntry_Click(object sender, RoutedEventArgs e)
+        private void Button_MainPage_Click(object sender, RoutedEventArgs e)
         {
-            var newForm = new Window_NewEntry();        //create your new form.
+            var newForm = new MainWindow();             //create your new form.
             newForm.Show();                             //show the new form.
             this.Close();                               //only if you want to close the current form.
         }
