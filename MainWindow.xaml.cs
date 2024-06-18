c6bf92820176922fdd4974b079c67ba36488cedb
Martin Schommertz <martinschommertz@googlemail.com>
2024-06-18 12:30:27 +0200

    Kommentare eingefügt für bessere Übersicht auf der Startseite.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,36 +13,37 @@ using System.Windows.Shapes;
 
 namespace MartinsHaushaltsbuch
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        //---------------------- Initialisierung der Seite und Integration des Singleton ----------------------
         public MainWindow()
         {
             InitializeComponent();
             DataContext = Singleton_Filter.Instance;
         }
 
+        //---------------------- Funktion für Navigation auf Seite "Buchungen" ----------------------
         private void Button_NewEntry_Click(object sender, RoutedEventArgs e)
         {
-            var newForm = new Window_NewEntry();        //create your new form.
-            newForm.Show();                             //show the new form.
-            this.Close();                               //only if you want to close the current form.
+            var newForm = new Window_NewEntry();
+            newForm.Show();
+            this.Close();
         }
 
+        //---------------------- Funktion für Navigation auf Seite "Einstellungen" ----------------------
         private void Button_Settings_Click(object sender, RoutedEventArgs e)
         {
-            var newForm = new Window_Settings();             //create your new form.
-            newForm.Show();                             //show the new form.
-            this.Close();                               //only if you want to close the current form.
+            var newForm = new Window_Settings();
+            newForm.Show();
+            this.Close();
         }
 
+        //---------------------- Funktion für Navigation auf Seite "Analyse" ----------------------
         private void Button_Analysis_Click(object sender, RoutedEventArgs e)
         {
-            var newForm = new Window_Analysis();             //create your new form.
-            newForm.Show();                             //show the new form.
-            this.Close();                               //only if you want to close the current form.
+            var newForm = new Window_Analysis();
+            newForm.Show();
+            this.Close();
         }
     }
 }
